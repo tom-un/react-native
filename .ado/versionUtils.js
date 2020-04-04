1e2c329a34a980d10c4124c4027713a41d51563a
Tom Underhill <tomun@microsoft.com>
2020-04-04 15:51:32 -0700

    Clean up merge markers

--- .ado/versionUtils.js
+++ .ado/versionUtils.js
@@ -14,23 +14,6 @@ function gatherVersionInfo() {
     return {pkgJson, releaseVersion, branchVersionSuffix};
 }
 
-function updateReactNativeMacOSInitVersionInFile() {
-    const rnMacOSInitPkgJsonPath = path.resolve(__dirname, "../packages/react-native-macos-init/package.json");
-    const rnMacOSInitPkgJson = JSON.parse(fs.readFileSync(rnMacOSInitPkgJsonPath, "utf8"));
-    let rnMacOSInitReleaseVersion = rnMacOSInitPkgJson.version;
-    const rnMacOSInitVersionStringRegEx = new RegExp(`([0-9]*)\\.([0-9]*)\\.([0-9]*)`);
-    const rnMacOSInitVersionGroups = rnMacOSInitVersionStringRegEx.exec(rnMacOSInitReleaseVersion);
-    if (rnMacOSInitVersionGroups) {
-      rnMacOSInitReleaseVersion = rnMacOSInitVersionGroups[1] + '.' + rnMacOSInitVersionGroups[2] + '.' + (parseInt(rnMacOSInitVersionGroups[3]) + 1);
-    } else {
-      console.log("Invalid react-native-macos-init version to publish");
-      process.exit(1);
-    }
-    rnMacOSInitPkgJson.version = rnMacOSInitReleaseVersion;
-    fs.writeFileSync(rnMacOSInitPkgJsonPath, JSON.stringify(rnMacOSInitPkgJson, null, 2));
-    console.log(`Updating ${rnMacOSInitPkgJsonPath} to version ${rnMacOSInitReleaseVersion}`);
-}
-
 function updateVersionsInFiles() {
 
     let {pkgJson, releaseVersion, branchVersionSuffix} = gatherVersionInfo();
@@ -51,11 +34,6 @@ function updateVersionsInFiles() {
     pkgJson.version = releaseVersion;
     fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
     console.log(`Updating package.json to version ${releaseVersion}`);
-<<<<<<< HEAD
-
-    updateReactNativeMacOSInitVersionInFile();
-=======
->>>>>>> ms/master
   
     return {releaseVersion, branchVersionSuffix};
 }
